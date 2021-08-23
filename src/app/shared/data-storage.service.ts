b37592ad61752066dd01145a86b63e3d30cddd95
Emmanuel Sunday Akomaning <sundux7@gmail.com>
2021-08-23 18:21:36 +0000

    completed resolver for data-storage

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
 import { Recipe } from './../recipes/recipe.model';
 import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { exhaustMap, map, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService{
     private server = "https://recipe-4a90c-default-rtdb.firebaseio.com";
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     public storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -19,16 +20,36 @@ export class DataStorageService{
     }
 
     public fetchRecipes() {
-        return this.http.get<Recipe[]>(`${this.server}/recipe.json`)
-        .pipe(
-            map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
-                })
-            }),
-            tap(recipes => {
-                this.recipeService.setRecipes(recipes); 
-            })
-        )
+        // trying to take only one property fron the user (BehaviouralSubject) from 
+        // the authService and then unscubscribe automativally
+        return this.authService.user.pipe(take(1), exhaustMap(user => {
+            return this.http
+            .get<Recipe[]>(`${this.server}/recipe.json`, 
+            {
+                params: new HttpParams().set('auth', user.token)
+            }
+            );
+        }), map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            });
+        }),
+        tap(recipes => {
+            this.recipeService.setRecipes(recipes); 
+        }));
     }
+
+    // public fetchRecipes() {
+    //     return this.http.get<Recipe[]>(`${this.server}/recipe.json`)
+    //     .pipe(
+    //         map(recipes => {
+    //             return recipes.map(recipe => {
+    //                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+    //             });
+    //         }),
+    //         tap(recipes => {
+    //             this.recipeService.setRecipes(recipes); 
+    //         })
+    //     )
+    // }
 }
